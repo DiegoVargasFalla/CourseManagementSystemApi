90e89b5bffc935a5cb28f63fd3635913ece1d16f
DiegoVargasFalla <fallaandres1007f@gmail.com>
2025-08-21 23:41:27 -0300

    authorization filter

--- src/main/java/com/ubbackend/security/filters/JwtAuthorizationFilter.java
+++ src/main/java/com/ubbackend/security/filters/JwtAuthorizationFilter.java
@@ -1,4 +1,53 @@
 package com.ubbackend.security.filters;
 
-public class JwtAuthorizationFilter {
+import com.ubbackend.security.jwt.JwtUtils;
+import com.ubbackend.servicesImpl.UserDetailsServiceImpl;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+    private final JwtUtils jwtUtils;
+    private final UserDetailsServiceImpl userDetailsServiceImpl ;
+
+    public JwtAuthorizationFilter(JwtUtils jwtUtils, UserDetailsServiceImpl userDetailsServiceImpl) {
+        this.jwtUtils = jwtUtils;
+        this.userDetailsServiceImpl = userDetailsServiceImpl;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain) throws ServletException, IOException {
+
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String tokenAuthHeader = authHeader.substring(7);
+
+            if(jwtUtils.validateToken(tokenAuthHeader)) {
+
+                String email = jwtUtils.getEmailFromToken(tokenAuthHeader);
+                UserDetails userDetails = userDetailsServiceImpl.loadUserByUsername(email);
+
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities()
+                );
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
 }
