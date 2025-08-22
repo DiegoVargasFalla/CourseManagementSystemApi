6be7d92cf734ad9feb8d517837b4deb502099b1a
DiegoVargasFalla <fallaandres1007f@gmail.com>
2025-08-21 23:40:28 -0300

    update configs

--- src/main/java/com/ubbackend/security/filters/JwtAuthenticationFilter.java
+++ src/main/java/com/ubbackend/security/filters/JwtAuthenticationFilter.java
@@ -1,4 +1,101 @@
 package com.ubbackend.security.filters;
 
-public class JwtAtutheticationFilter {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ubbackend.DTO.UserLoginDTO;
+import com.ubbackend.security.jwt.JwtUtils;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private final JwtUtils jwtUtils;
+
+    public JwtAuthenticationFilter(JwtUtils jwtUtils) {
+        this.jwtUtils = jwtUtils;
+    }
+
+    public UserLoginDTO parseUserLoginDTO(HttpServletRequest request) {
+        try {
+            System.out.println("-> Map UserLogin");
+
+            //mapping of user whit library jackson
+            return new ObjectMapper().readValue(request.getInputStream(), UserLoginDTO.class);
+        } catch (IOException e) {
+            throw new RuntimeException("Error parsing login request" + e.getMessage());
+        }
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request,
+                                                HttpServletResponse response) throws AuthenticationException {
+
+        try {
+            UserLoginDTO userLoginDTO = parseUserLoginDTO(request);
+            System.out.println("-> ### After mapping ####");
+            System.out.println("-> userLoginDTO: " + userLoginDTO.getEmail() + " ---- " + userLoginDTO.getPassword());
+
+            UsernamePasswordAuthenticationToken authenticationToken =
+                    new UsernamePasswordAuthenticationToken(
+                            userLoginDTO.getEmail(),
+                            userLoginDTO.getPassword()
+                    );
+
+            return getAuthenticationManager().authenticate(authenticationToken);
+
+        } catch (RuntimeException e) {
+            throw new BadCredentialsException("Invalid login request format") {};
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication authResult) throws IOException {
+
+        User user = (User) authResult.getPrincipal();
+        String token = jwtUtils.generateToken(user);
+
+        response.addHeader("Authorization", token);
+
+        Map<String, Object> httpResponse = new HashMap<>();
+        httpResponse.put("token", token);
+        httpResponse.put("Message", "authenticated");
+        httpResponse.put("email", user.getUsername());
+
+        response.getWriter().write(new ObjectMapper().writeValueAsString(httpResponse));
+        response.setStatus(HttpStatus.OK.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getWriter().flush();
+
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+
+        logger.error("Authentication failed: " + failed.getMessage());
+
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        Map<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("error", "Authentication failed");
+        errorResponse.put("message", failed.getMessage());
+
+        response.getWriter().write(new ObjectMapper().writeValueAsString(errorResponse));
+        response.getWriter().flush();
+    }
 }
