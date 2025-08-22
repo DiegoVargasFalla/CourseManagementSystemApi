ac73072c808ce615a402a2f25bcaf72d9ebed01f
DiegoVargasFalla <fallaandres1007f@gmail.com>
2025-08-21 23:38:33 -0300

    apdate cors

--- src/main/java/com/ubbackend/security/CorsConfig.java
+++ src/main/java/com/ubbackend/security/CorsConfig.java
@@ -20,7 +20,7 @@ public class CorsConfig {
         config.setAllowedOrigins(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(List.of("*"));
-        config.setAllowCredentials(false);
+        config.setAllowCredentials(true);
 
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
