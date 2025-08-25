cdf5e2bae4dd290311b8bace2b14ebdd1f737ddd
DiegoVargasFalla <fallaandres1007f@gmail.com>
2025-08-25 19:56:39 -0300

    create service mailSender

--- src/main/java/com/ubbackend/servicesImpl/MailSenderImpl.java
+++ src/main/java/com/ubbackend/servicesImpl/MailSenderImpl.java
@@ -1,4 +1,43 @@
 package com.ubbackend.servicesImpl;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import static org.antlr.v4.runtime.misc.Utils.readFile;
+
+@Service
 public class MailSenderImpl {
+
+    private final JavaMailSender mailSender;
+
+    public MailSenderImpl(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendMail(String to, String subject, String text) throws MessagingException, IOException {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+
+        mimeMessage.setFrom(new InternetAddress("cursosaub@gmail.com"));
+        mimeMessage.setRecipients(MimeMessage.RecipientType.TO, InternetAddress.parse(to));
+        mimeMessage.setSubject(subject);
+
+        char[] htmlTemplate = readFile("src/main/resources/static/index.html");
+        StringBuilder html = new StringBuilder();
+
+        for(char c : htmlTemplate) {
+            html.append(c);
+        }
+
+        String htmlString = html.toString().replace("%ACCESS_CODE%", text);
+
+        mimeMessage.setContent(htmlString, "text/html; charset=utf-8");
+        mailSender.send(mimeMessage);
+    }
+
+
 }
